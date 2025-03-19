24a651cb50849d0122bb6f6a0c9b3e706d67e684
Lenny ROUANET <lennyrouanet@users.noreply.github.com>
2025-03-19 08:55:02 +0100

    Fixtures

--- fixture/Entity/Transport/Email.php
+++ fixture/Entity/Transport/Email.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Fixture\Entity\Transport;
 
+use Phant\EmailSender\Service\EmailBuilder;
 use Phant\Notifier\Entity\Transport\Email as EmailTransportEntity;
-use Fixture\Port\EmailBuilder;
 use Fixture\Port\EmailSender;
 
 final class Email
